7e2141f04b8fd5492c0fd3fa05e3789558884523
Megha Zavar <mybiyani@gmail.com>
2022-09-20 17:59:27 -0600

    Add files via upload

---
+++ src/CollectorButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CollectorButton() {
+
+  return (
+    <div>
+      <button>
+        Click Here!
+      </button>
+    </div>
+  );
+}  
---
+++ src/RequestFormButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function RequestFormButton() {
+
+
+  return (
+    <div>
+      <input type="submit" value="Submit" />
+
+    </div>
+  );
+}
